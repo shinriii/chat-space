39b0f204ed81fc3e3c5e10c9974f4a7a2f365a8e
shinriii <q9s5z20o.1i@ezweb.ne.jp>
2019-12-25 19:17:15 +0900

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chat-space20191225'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space20191225'
 end
